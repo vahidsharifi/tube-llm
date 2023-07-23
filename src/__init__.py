6306a74064919f61861bfd3a11c86d9a1746e44f
Usama Shahid <usamashahid.us8@gmail.com>
2023-07-23 23:09:17 +0100

    chore: readme update

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,6 @@
 from .data_connection import Loader, Transformer, Store, Retriever
 from .models_io import LanguageModels, OutputParsers, Prompts
 from .chains import Chain
+from .memory import Memory
+from .agents import Agent
 from .question_answering import QuestionAnswering
