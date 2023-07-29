4079082e908198c729ff43df3aa2791206450887
Vahid Sharifi <s4217641@glos.ac.uk>
2023-07-29 18:17:26 +0100

    front-end & wandb added

--- src/question_answering/question_answering.py
+++ src/question_answering/question_answering.py
@@ -4,7 +4,7 @@ from langchain.prompts import PromptTemplate
 
 template = """Use the following pieces of context to answer the question at the end. 
 If you don't know the answer, just say that you don't know, don't try to make up an answer. 
-Use three sentences maximum and keep the answer as concise as possible. 
+Use six sentences maximum and keep the answer as concise as possible. 
 {context}
 Question: {question}
 Helpful Answer:"""
