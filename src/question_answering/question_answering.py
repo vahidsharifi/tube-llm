7a32635805c6d0d5f8bd53934302242e0a21bf7b
Vahid Sharifi <s4217641@glos.ac.uk>
2023-08-01 15:49:11 +0100

    Update question_answering.py

--- src/question_answering/question_answering.py
+++ src/question_answering/question_answering.py
@@ -1,13 +1,16 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
-from langchain.prompts import PromptTemplate
-
+from langchain.prompts import PromptTemplate 
 template = """Use the following pieces of context to answer the question at the end. 
-If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+If you don't know the answer, just say that you don't know, don't try to make up an answer.\
 Use six sentences maximum and keep the answer as concise as possible. 
-{context}
+
+Context:{context}
+
 Question: {question}
+
 Helpful Answer:"""
+
 QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"], template=template, )
 
 
