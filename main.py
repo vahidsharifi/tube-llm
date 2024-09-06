17eda5415d387adc39d1f07fcd47be5c9852b2b8
Vahid Sharifi <s4217641@glos.ac.uk>
2024-09-06 12:22:29 +0100

    first attempt to fix Deployment port error

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ app = FastAPI(debug=True)
 
 # Configure CORS to allow requests from the frontend (replace * with your frontend URL)
 origins = ["*"]
-# app.add_middleware(CORSMiddleware, allow_origins=origins, allow_methods=["*"], allow_headers=["*"])
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allows all origins
@@ -19,13 +18,18 @@ app.add_middleware(
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
+
 # Define the project root and mount the static files directory
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="static")
 app.include_router(answer_router, prefix="", tags=["Answer"])
 
-
 @app.get("/", response_class=HTMLResponse)
 async def read_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+# Add this block to ensure the app listens on the port provided by the environment variable
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8080))  # Get the PORT from the environment, default to 8080
+    uvicorn.run(app, host="0.0.0.0", port=port)
